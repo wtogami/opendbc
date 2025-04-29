2c49a3dcdb76bb06c3b89482ff2188042c8a80a7
rav4kumar <meetkumardesai@gmail.com>
2025-04-28 21:42:59 -0700

    custm tune
    low speed
    
    Update interface.py
    
    ff
    
    ff
    
    tune
    
    Add new ACC steering wheel message for SecOC cars. Update distance_button with the new message.
    
    Update carcontroller.py
    
    tt
    
    t

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -1,3 +1,4 @@
+from openpilot.common.params import Params
 from opendbc.car import Bus, structs, get_safety_config, uds
 from opendbc.car.toyota.carstate import CarState
 from opendbc.car.toyota.carcontroller import CarController
@@ -150,13 +151,24 @@ class CarInterface(CarInterfaceBase):
     # min speed to enable ACC. if car can do stop and go, then set enabling speed
     # to a negative value, so it won't matter.
     ret.minEnableSpeed = -1. if stop_and_go else MIN_ACC_SPEED
+    sp_tss2_long_tune = Params().get_bool("ToyotaTSS2Long")
 
     if candidate in TSS2_CAR:
       ret.flags |= ToyotaFlags.RAISED_ACCEL_LIMIT.value
-
-      ret.vEgoStopping = 0.25
-      ret.vEgoStarting = 0.25
-      ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+      if sp_tss2_long_tune:
+        ret.vEgoStopping = 0.25
+        ret.vEgoStarting = 0.25
+        if candidate == CAR.TOYOTA_RAV4_TSS2:
+          ret.stoppingDecelRate = 0.05  # optimal on rav4
+        else:
+          ret.stoppingDecelRate = 0.02  # optimal on corollaH, prius
+      else:
+        ret.vEgoStopping = 0.25
+        ret.vEgoStarting = 0.25
+        if candidate == CAR.TOYOTA_RAV4_TSS2:
+          ret.stoppingDecelRate = 0.3  # optimal on rav4
+        else:
+          ret.stoppingDecelRate = 0.03  # optimal on corollaH, prius
 
       # Hybrids have much quicker longitudinal actuator response
       if ret.flags & ToyotaFlags.HYBRID.value:
