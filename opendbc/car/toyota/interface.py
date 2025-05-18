78f344272242d0a42a71575f70f7fc187ca1daef
rav4kumar <meetkumardesai@gmail.com>
2025-05-18 16:54:15 -0700

    final

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -159,16 +159,16 @@ class CarInterface(CarInterfaceBase):
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
         if candidate == CAR.TOYOTA_RAV4_TSS2:
-          ret.stoppingDecelRate = 0.05  # optimal on rav4
+          ret.stoppingDecelRate = 0.3  # optimal on rav4
         else:
-          ret.stoppingDecelRate = 0.02  # optimal on corollaH, prius
+          ret.stoppingDecelRate = 0.03  # optimal on corollaH, prius
       else:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
         if candidate == CAR.TOYOTA_RAV4_TSS2:
           ret.stoppingDecelRate = 0.3  # optimal on rav4
         else:
-          ret.stoppingDecelRate = 0.03  # optimal on corollaH, prius
+          ret.stoppingDecelRate = 0.05  # optimal on corollaH, prius
 
       # Hybrids have much quicker longitudinal actuator response
       if ret.flags & ToyotaFlags.HYBRID.value:
