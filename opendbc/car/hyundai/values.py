8d24c3292d715aeddef552b1fb9da25d42e7c120
Warren Togami <wtogami@gmail.com>
2025-05-27 22:50:57 -0500

    Revert "lower the ramp up and down rate"
    This reverts commit 351a498914f0f923fa61106dc32541d1625bfa7a.

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -32,8 +32,8 @@ class CarControllerParams:
   # 250 can at least achieve 4 m/s^2, 80 corresponds to ~2.5 m/s^2
   ANGLE_MAX_TORQUE = 250  # The maximum amount of torque that will be allowed
   ANGLE_MIN_TORQUE = 25  # equivalent to ~0.8 m/s^2 of torque (based on ANGLE_MAX_TORQUE) when overriding
-  ANGLE_RAMP_UP_RATE = 1  # Max rate of change of torque
-  ANGLE_RAMP_DOWN_RATE = 1  # Max rate of change of torque
+  ANGLE_RAMP_UP_RATE = 2  # Max rate of change of torque
+  ANGLE_RAMP_DOWN_RATE = 3  # Max rate of change of torque
   ANGLE_TORQUE_OVERRIDE_CYCLES = 17  # The number of cycles it takes to ramp down to min torque when the user intervenes.
   STEER_STEP = 1  # Angle command is sent at 100 Hz
 
